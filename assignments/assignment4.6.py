8222058489c740eb362928d98557737cdf204e77
unknown <zizzis@gmail.com>
2023-06-09 16:36:53 +0200

    refactor assignment4.6

--- assignments/assignment4.6.py
+++ assignments/assignment4.6.py
@@ -1,12 +1,15 @@
+def computepay(hours_worked, rate):
+    if hours_worked <= 40:
+        return hours_worked * rate
+    elif hours_worked > 40:
+        overtime = hours_worked - 40
+        overtime_rate = rate * 1.5
+        return (40 * rate) + (overtime * overtime_rate)
+
 hrs = input("Enter Hours:")
-h= float(hrs)
+h = float(hrs)
 rate = input("Enter Rates:")
-r= float(rate)
-def computepay():
-    if h<=40:
-        return h*r
-    elif h>40:
-        h2= h-40
-        r2= r*1.5
-        return (40*r)+(h2*r2)
-print("Pay", computepay())
+r = float(rate)
+
+print("Pay", computepay(h, r))
+
